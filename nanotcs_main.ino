76c54949feb52f37342917d33fbcfdd9022b52cd
jchaewon122 <chaewon5122@naver.com>
2025-09-03 03:53:45 +0900

    Update nanotcs_main.ino

--- nanotcs_main.ino
+++ nanotcs_main.ino
@@ -15,6 +15,10 @@ Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_154MS, TCS347
 String currentColorStatus = "Other";
 bool tcsSensorFound = false;
 
+// New variable for continuous reading
+unsigned long lastSensorReadTime = 0;
+const unsigned long SENSOR_READ_INTERVAL = 50; // 50ms interval for continuous reading
+
 void setup() {
   Serial.begin(9600); // For PC debug monitor
   orangeBleSerial.begin(9600);
@@ -32,6 +36,38 @@ void setup() {
 }
 
 void loop() {
+  // Continuously read and print sensor data for testing
+  unsigned long currentTime = millis();
+  if (currentTime - lastSensorReadTime >= SENSOR_READ_INTERVAL) {
+    lastSensorReadTime = currentTime;
+
+    if (tcsSensorFound) {
+      uint16_t r, g, b, c;
+      tcs.getRawData(&r, &g, &b, &c);
+
+      long rgbSum = r + g + b;
+
+      // Classify color as "Black" or "White" based on RGB sum
+      // --- Modified Line ---
+      if (rgbSum < 1600) { 
+        currentColorStatus = "Black";
+      } else {
+        currentColorStatus = "White";
+      }
+      // --- End of Modified Line ---
+
+      // Print to Serial Monitor for debug
+      Serial.print("R: "); Serial.print(r);
+      Serial.print(", G: "); Serial.print(g);
+      Serial.print(", B: "); Serial.print(b);
+      Serial.print(", C: "); Serial.print(c);
+      Serial.print(" -> Color: "); Serial.println(currentColorStatus);
+    } else {
+      currentColorStatus = "Error"; // Sensor initialization failed
+      Serial.println("R: Error, G: Error, B: Error, C: Error -> Color: Error");
+    }
+  }
+
   // Listen for command from master (Orange Board)
   if (orangeBleSerial.available()) {
     String command = orangeBleSerial.readStringUntil('\n');
@@ -39,31 +75,6 @@ void loop() {
 
     // If the command is "GET_TCS", update and send data
     if (command == "GET_TCS") {
-      if (tcsSensorFound) {
-        uint16_t r, g, b, c;
-        tcs.getRawData(&r, &g, &b, &c);
-
-        long rgbSum = r + g + b;
-
-        // Classify color as "Black" or "White" based on RGB sum
-        if (rgbSum < 3000) {
-          currentColorStatus = "Black";
-        } else {
-          currentColorStatus = "White";
-        }
-
-        // Print to Serial Monitor for debug
-        Serial.print("R: "); Serial.print(r);
-        Serial.print(", G: "); Serial.print(g);
-        Serial.print(", B: "); Serial.print(b);
-        Serial.print(", C: "); Serial.print(c);
-        Serial.print(" -> Color: "); Serial.println(currentColorStatus);
-
-      } else {
-        currentColorStatus = "Error"; // Sensor initialization failed
-        Serial.println("R: Error, G: Error, B: Error, C: Error -> Color: Error");
-      }
-
       // Transmit data to Orange BLE with a unique ID
       orangeBleSerial.print("NANO_TCS:");
       orangeBleSerial.println(currentColorStatus);
