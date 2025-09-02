1c8063ea62263d0cb5f1248b054db4b34838a080
jchaewon122 <chaewon5122@naver.com>
2025-09-03 05:18:36 +0900

    Update nanotcs_main.ino

--- nanotcs_main.ino
+++ nanotcs_main.ino
@@ -2,17 +2,22 @@
 #include <Wire.h>
 #include <Adafruit_TCS34725.h>
 
-SoftwareSerial orangeBleSerial(2, 3);
+SoftwareSerial orangeSerial(2, 3);
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_154MS, TCS34725_GAIN_16X);
+
+#define SLAVE_ID 2
 
 String currentColorStatus = "Other";
 bool tcsSensorFound = false;
 unsigned long lastSensorReadTime = 0;
-const unsigned long SENSOR_READ_INTERVAL = 50;
+const unsigned long SENSOR_READ_INTERVAL = 100;
+
+void updateSensorData();
+void sendDataToMaster();
 
 void setup() {
   Serial.begin(9600);
-  orangeBleSerial.begin(9600);
+  orangeSerial.begin(9600);
 
   if (tcs.begin()) {
     tcsSensorFound = true;
@@ -22,42 +27,63 @@ void setup() {
     Serial.println("NANO_TCS: No TCS34725 sensor found!");
   }
 
-  Serial.println("NANO_TCS Slave initialized");
+  Serial.print("NANO_TCS Slave initialized with ID: ");
+  Serial.println(SLAVE_ID);
 }
 
 void loop() {
-  unsigned long currentTime = millis();
-  if (currentTime - lastSensorReadTime >= SENSOR_READ_INTERVAL) {
-    lastSensorReadTime = currentTime;
-    if (tcsSensorFound) {
-      uint16_t r, g, b, c;
-      tcs.getRawData(&r, &g, &b, &c);
-      long rgbSum = r + g + b;
+  // Check for commands from master
+  if (orangeSerial.available()) {
+    String command = orangeSerial.readStringUntil('\n');
+    command.trim();
 
-      if (rgbSum < 1600) {
-        currentColorStatus = "Black";
-      } else {
-        currentColorStatus = "White";
-      }
-
-      Serial.print("R: "); Serial.print(r);
-      Serial.print(", G: "); Serial.print(g);
-      Serial.print(", B: "); Serial.print(b);
-      Serial.print(", C: "); Serial.print(c);
-      Serial.print(" -> Color: "); Serial.println(currentColorStatus);
-    } else {
-      currentColorStatus = "Error";
-      Serial.println("R: Error, G: Error, B: Error, C: Error -> Color: Error");
+    // Check for data request
+    if (command == "REQ_DATA") {
+      sendDataToMaster();
     }
   }
 
-  if (orangeBleSerial.available()) {
-    String command = orangeBleSerial.readStringUntil('\n');
-    command.trim();
+  // Periodic sensor reading
+  unsigned long currentTime = millis();
+  if (currentTime - lastSensorReadTime >= SENSOR_READ_INTERVAL) {
+    lastSensorReadTime = currentTime;
+    updateSensorData();
+  }
+}
 
-    if (command == "GET_TCS") {
-      orangeBleSerial.print("NANO_TCS:");
-      orangeBleSerial.println(currentColorStatus);
+void sendDataToMaster() {
+  orangeSerial.print("TCS:");
+  orangeSerial.println(currentColorStatus);
+  
+  // 전송 완료 대기
+  orangeSerial.flush();
+  
+  Serial.print("Sent to master - Color: ");
+  Serial.println(currentColorStatus);
+}
+
+void updateSensorData() {
+  if (tcsSensorFound) {
+    uint16_t r, g, b, c;
+    tcs.getRawData(&r, &g, &b, &c);
+    
+    // 센서 에러 체크
+    if (c == 0) {
+      currentColorStatus = "Error";
+      return;
     }
+    
+    long rgbSum = r + g + b;
+
+    // 임계값 조정 및 추가 검증
+    if (rgbSum < 1600) {
+      currentColorStatus = "Black";
+    } else if (rgbSum > 1600) {
+      currentColorStatus = "White";
+    } else {
+      currentColorStatus = "Other";
+    }
+  } else {
+    currentColorStatus = "Error";
   }
 }
